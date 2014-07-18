2d679da89f5155e9660cce7449226508200ee87f
ZiYang <robertsky.zhang@gmail.com>
2014-07-18 01:36:19 -0400

    changed UI

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('new message', msg);
