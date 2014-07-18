4f50bf8e8c6dd1dcc9a9ec0ba66cba4ba1acf4fc
ZiYang <robertsky.zhang@gmail.com>
2014-07-17 22:29:54 -0400

    erase config

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
-io.configure(function () {
-    io.set("transports", ["xhr-polling"]);
-    io.set("polling duration", 10);
-});
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('new message', msg);
